d62707ca71892553ef76714e7152fdd63d19c3eb
Joshua Koshy <contact@koshy.dev>
2022-06-27 12:56:12 -0400

    Initial Commit

---
+++ vp2/vp2App.swift
@@ -0,0 +1,17 @@
+//
+//  vp2App.swift
+//  vp2
+//
+//  Created by Joshua Koshy on 6/27/22.
+//
+
+import SwiftUI
+
+@main
+struct vp2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
